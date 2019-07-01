f05f0b050fed89675f60ca70bf20981ccdd0dabd
jrm1715 <rocknrollx18@aim.com>
2019-06-30 18:33:20 -0700

    Added new routes for comment functionality as well as implemented the ability to add a comment to a specific post.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require("express"),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comments"),
     seedDB = require("./seeds")
 
 seedDB();
@@ -19,7 +20,7 @@ app.get("/campgrounds", function(req, res) {
     if(err) {
       console.log(err);
     } else {
-      res.render("index", {campgrounds: allCampgrounds});
+      res.render("campgrounds/index", {campgrounds: allCampgrounds});
     }
   });
 });
@@ -39,7 +40,7 @@ app.post("/campgrounds", function(req, res) {
 });
 
 app.get("/campgrounds/new", function(req, res) {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function(req, res) {
@@ -47,9 +48,46 @@ app.get("/campgrounds/:id", function(req, res) {
     if(err) {
       console.log(err);
     } else {
-      res.render("show", {campground: foundCampground});
+      res.render("campgrounds/show", {campground: foundCampground});
     }
   });
+});
+
+// ============================
+// COMMENTS ROUTES
+// ============================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  //look up camground using ID
+  Campground.findById(req.params.id, function(err, campground){
+    if (err) {
+      console.log(err);
+      res.redirect("/camogrounds");
+    } else {
+      Comment.create(req.body.comment, function(err, comment){
+        if (err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+  //create new comment
+  //connect new comment to campground
+  //redirect campground show page
 });
 
 app.listen(3000, function() {
