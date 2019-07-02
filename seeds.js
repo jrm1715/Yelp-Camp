3906eb44ba927d266c84e84aedca04788349c92e
jrm1715 <rocknrollx18@aim.com>
2019-07-01 22:00:15 -0700

    sylized show page and the campground page.

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ let data = [
   {
     name: "Clouds Rest",
     image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1388&q=80",
-    description: "blah blah blah"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dapibus enim eget rutrum ullamcorper. Donec tincidunt sed diam id mollis. Maecenas convallis lorem vel lacus molestie consectetur. Aliquam laoreet faucibus enim, id placerat urna porta eu. Sed viverra sollicitudin ex, in ornare ante porttitor quis. Fusce magna metus, porttitor vel pulvinar vitae, bibendum a nisi. Nulla ultricies ullamcorper ipsum eget tincidunt."
   },
   {
     name: "Eagle Creek",
     image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-    description: "blah blah blah"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dapibus enim eget rutrum ullamcorper. Donec tincidunt sed diam id mollis. Maecenas convallis lorem vel lacus molestie consectetur. Aliquam laoreet faucibus enim, id placerat urna porta eu. Sed viverra sollicitudin ex, in ornare ante porttitor quis. Fusce magna metus, porttitor vel pulvinar vitae, bibendum a nisi. Nulla ultricies ullamcorper ipsum eget tincidunt."
   },
   {
     name: "Scouts Ridge",
     image: "https://images.unsplash.com/photo-1535049883634-993346531df6?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-    description: "blah blah blah"
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dapibus enim eget rutrum ullamcorper. Donec tincidunt sed diam id mollis. Maecenas convallis lorem vel lacus molestie consectetur. Aliquam laoreet faucibus enim, id placerat urna porta eu. Sed viverra sollicitudin ex, in ornare ante porttitor quis. Fusce magna metus, porttitor vel pulvinar vitae, bibendum a nisi. Nulla ultricies ullamcorper ipsum eget tincidunt."
   }
 ]
 
